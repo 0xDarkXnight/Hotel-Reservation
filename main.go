d35fcbf34d0cc237d02fce2f48d14f8c5e439768
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-12-16 19:53:15 +0530

    MongoDB integration

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/0xDarkXnight/Hotel-Reservation-Site/api"
+	"github.com/0xDarkXnight/Hotel-Reservation-Site/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	dburi    = "mongodb://localhost:27017"
+	dbname   = "hotel-reservation"
+	userColl = "users"
 )
 
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	user := types.User{
+		FirstName: "James",
+		LastName:  "Hugh",
+	}
+	coll := client.Database(dbname).Collection(userColl)
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 

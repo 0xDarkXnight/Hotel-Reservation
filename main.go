b1b77ecf6d5c8656f2f41c7be85e7fd740ff402b
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-12-18 18:04:04 +0530

    MongoDB Integration

--- main.go
+++ main.go
@@ -3,47 +3,41 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/0xDarkXnight/Hotel-Reservation-Site/api"
-	"github.com/0xDarkXnight/Hotel-Reservation-Site/types"
+	"github.com/0xDarkXnight/Hotel-Reservation-Site/db"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
-	dburi    = "mongodb://localhost:27017"
-	dbname   = "hotel-reservation"
-	userColl = "users"
+	dburi = "mongodb://localhost:27017"
 )
 
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	flag.Parse()
+
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	user := types.User{
-		FirstName: "James",
-		LastName:  "Hugh",
-	}
-	coll := client.Database(dbname).Collection(userColl)
-	res, err := coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
+	// handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
-	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
-	flag.Parse()
-
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }

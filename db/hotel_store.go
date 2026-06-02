f31da0ea75a7bb916c9a02de5c1b100ae7c9800e
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2026-06-02 19:43:46 +0530

    fix: updating hotel with rooms booked

--- db/hotel_store.go
+++ db/hotel_store.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/0xDarkXnight/Hotel-Reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -16,6 +17,7 @@ type HotelStore interface {
 	// GetHotelByID(context.Context, string) (*types.User, error)
 	// GetHotels(context.Context) ([]*types.Hotel, error)
 	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Update(context.Context, bson.M, bson.M) error
 	// DeleteHotel(context.Context, string) error
 	// UpdateHotel(context.Context, bson.M, types.UpdateHotelParams)
 }
@@ -25,10 +27,10 @@ type MongoHotelStore struct {
 	coll   *mongo.Collection
 }
 
-func NewMongoHotelStore(client *mongo.Client, dbname string) *MongoHotelStore {
+func NewMongoHotelStore(client *mongo.Client) *MongoHotelStore {
 	return &MongoHotelStore{
 		client: client,
-		coll:   client.Database(dbname).Collection(hotelColl),
+		coll:   client.Database(DBNAME).Collection(hotelColl),
 	}
 }
 
@@ -39,4 +41,12 @@ func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (
 	}
 	hotel.ID = res.InsertedID.(primitive.ObjectID)
 	return hotel, nil
+}
+
+func (s *MongoHotelStore) Update(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := s.coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
 }

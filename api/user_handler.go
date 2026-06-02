cb143eca30e63edb85d8800d75b5874c9eacd550
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2026-06-02 18:15:56 +0530

    feat: add delete user functionality

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"errors"
+
 	"github.com/0xDarkXnight/Hotel-Reservation/db"
 	"github.com/0xDarkXnight/Hotel-Reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserHandler struct {
@@ -14,6 +17,28 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	return &UserHandler{
 		userStore: userStore,
 	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id = c.Params("id")
+	)
+	user, err := h.userStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.JSON(map[string]string{"error": "user not found"})
+		}
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
 }
 
 func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
@@ -35,21 +60,10 @@ func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
 	return c.JSON(insertedUser)
 }
 
-func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
-	var (
-		id = c.Params("id")
-	)
-	user, err := h.userStore.GetUserByID(c.Context(), id)
-	if err != nil {
+func (h *UserHandler) HandleDeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	if err := h.userStore.DeleteUser(c.Context(), userID); err != nil {
 		return err
 	}
-	return c.JSON(user)
-}
-
-func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
-	users, err := h.userStore.GetUsers(c.Context())
-	if err != nil {
-		return err
-	}
-	return c.JSON(users)
+	return c.JSON(map[string]string{"deleted": userID})
 }

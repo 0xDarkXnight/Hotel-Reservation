8774399930cece0fc53217579560bb26eca105fc
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2026-06-02 20:52:39 +0530

    feat: add get booking by id functionality

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -24,3 +24,13 @@ func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
 	}
 	return c.JSON(bookings)
 }
+
+// TODO: this needs to be user authorized!
+func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
+	id := c.Params("id")
+	booking, err := h.store.BookingStore.GetBookingByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(booking)
+}
